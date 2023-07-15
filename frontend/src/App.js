a459e38c66fc36a20a08c528ccb09d369e6d91e8
Kanailal Manna <91842029+KanailalManna@users.noreply.github.com>
2023-07-15 10:13:09 +0530

    working on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
-import {BrowserRouter as Router} from "react-router-dom"
-import Header from "./components/Header"
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import Header from "../src/components/layout/Header/Header"
+import Footer from './components/layout/Footer/Footer';
+import React from 'react';
+// import WebFont from 'webfontloader';
+import { useEffect } from 'react';
+
 
 function App() {
+
+  // useEffect(() => {
+  //   WebFont.load({
+  //     google: {
+  //       families: ["Roboto", "Droid Sans", "Chilanka"],
+  //     },
+  //   });
+  // }, []);
+
+
+
   return (
     <Router>
-      <Header/>
+      <Header/>     {/*need to add icons*/}
+
+
+      <Footer/>
     </Router>
+
   );
 }
 
