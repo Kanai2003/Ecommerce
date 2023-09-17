c18286b94631563d1388b24aa673d5141ec93a70
Kanai <kanailalmanna2003@gmail.com>
2023-09-17 20:10:01 +0530

    Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,19 @@ import {BrowserRouter as Router, Route} from "react-router-dom"
 import Header from "../src/components/layout/Header/Header"
 import Footer from './components/layout/Footer/Footer';
 import React from 'react';
-// import WebFont from 'webfontloader';
+import WebFont from 'webfontloader';
 import { useEffect } from 'react';
 
 
 function App() {
 
-  // useEffect(() => {
-  //   WebFont.load({
-  //     google: {
-  //       families: ["Roboto", "Droid Sans", "Chilanka"],
-  //     },
-  //   });
-  // }, []);
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
 
 
 
