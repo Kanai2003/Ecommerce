3f7d7062bc3ad6ae2a7e6b42cf83dba376213e24
Kanai <kanailalmanna2003@gmail.com>
2023-09-22 15:59:36 +0530

    🛣️ on going

--- frontend/src/components/layout/NotFound/NotFound.js
+++ frontend/src/components/layout/NotFound/NotFound.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ErrorIcon from '@material-ui/icons/Error';
 import "./NotFound.css";
-import {Typography} from "@material-ui/core";
-import {Link} from "react-router-dom";
+import { Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
     return (
         <div className="PageNotFound">
-            <ErrorIcon/>
+            <ErrorIcon />
+
+            <Typography>Page Not Found </Typography>
             <Link to="/">Home</Link>
         </div>
     );
 }
+
+export default NotFound;
